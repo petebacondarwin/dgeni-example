6bddb24f5f83469b4086a9f360b623e7120aa15d
Peter Bacon Darwin <pete@bacondarwin.com>
2014-07-24 15:39:35 +0100

    refact(Gruntfile): update to no-config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,11 @@
+var Dgeni = require('dgeni');
+
 module.exports = function(grunt) {
 
   grunt.registerTask('dgeni', 'Generate docs via dgeni.', function() {
-    var dgeni = require('dgeni');
     var done = this.async();
-
-    var generateDocs = dgeni.generator('docs/dgeni.conf.js');
-    generateDocs().then(done);
+    var dgeni = new Dgeni([require('./docs/dgeni-example')]);
+    dgeni.generate().then(done);
   });
 
   grunt.registerTask('default', ['dgeni']);
